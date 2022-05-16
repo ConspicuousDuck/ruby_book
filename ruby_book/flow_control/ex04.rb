3ef025a48a30b7fbab98a0cb427ac12b41172909
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-15 23:09:21 -0700

    morp

---
+++ ruby_book/flow_control/ex04.rb
@@ -0,0 +1,43 @@
+# flow_control_ex04.rb
+
+=begin
+
+snip1:
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+  
+  "FALSE" ##answer
+  This is an if statement writtin in ternary operation form
+
+snip2: 
+x = 2
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+  puts "Did you get it right?"
+else
+  puts "Did you?"
+end
+
+  6 / 2 == 8 - 2 - 3
+  3 == 3
+  true   
+  "Did you get it right?" ##answer
+
+snip3:
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "Alright."
+elsif (x + 1) >= (y)
+  puts "Alright now!"
+elsif (y + 1) == x
+  puts "ALRIGHT NOW!"
+else
+  puts "Alrighty!"
+end
+
+  11 <= 9 false
+  11 >= 10 true
+  10 == 10 true
+  "Alright now!" ##answer
+
+
+=end
