6693b20f97837514a0c04afaa37ccb8d75282ea9
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-16 17:46:54 -0700

    done

---
+++ ruby_book/flow_control/ex05.rb
@@ -0,0 +1,6 @@
+# flow_control_ex05.rb
+
+# You get the error because the there is a syntax error.
+  # The code thinks the 'end' keyword is for the if statement
+  # and not for the method declaration 
+# You can fix it by adding "end" for the if statement
