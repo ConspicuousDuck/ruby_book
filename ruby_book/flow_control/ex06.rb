a78f2768b5e65f6653e8806e930b713f1dc221e7
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-16 17:54:07 -0700

    done

---
+++ ruby_book/flow_control/ex06.rb
@@ -0,0 +1,23 @@
+# flow_control_ex06.rb
+
+=begin
+
+(32 * 4) >= "129"
+  128 >= "129"
+  error. can't compare int and string
+
+847 == '874'
+  error. can't compare int and string
+
+'847' < '846'
+  false.
+
+'847' > '846'
+  true.
+
+'847' > '8478'
+  false.
+
+'847' < '8478'
+  true.  
+=end 
