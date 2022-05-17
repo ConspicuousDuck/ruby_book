20b6cdf700b59889bca7ee1ca3055e0c59dd7ee4
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-16 21:27:09 -0700

    beep beep

---
+++ ruby_book/loops_iterators/ex01.rb
@@ -0,0 +1,5 @@
+# loops_iterators_ex01.rb
+
+# it returns [1,2,3,4,5] 
+# .each method always returns the original array
+# check the documentation of .each method!
---
+++ ruby_book/loops_iterators/ex02.rb
@@ -0,0 +1,9 @@
+# loops_iterators_ex02.rb
+
+answer = nil
+
+while(answer != "STOP")
+  puts "Beep beep."
+  puts "Want more beeps?! Enter 'STOP' to end."
+  answer = gets.chomp
+end
