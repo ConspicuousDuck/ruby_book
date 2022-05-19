044a4486c5a6932c3d8a31275db6e961349c1fbe
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-19 00:39:59 -0700

    coo git push

---
+++ ruby_book/hash/ex01.rb
@@ -0,0 +1,17 @@
+# hash_ex01.rb
+
+
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+your_family = family.select { |k, v| (k == :sisters) || (k == :brothers) }
+
+your_family.values.flatten
+
+#.values returns an array with  only the values of our sisters: 
+  and brothers: from the hash.
+#.flatten made our nested array into 1 array.
+
