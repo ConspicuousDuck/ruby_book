a8c6c24341a11350195af61924f4b8f1e85bb8cc
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-19 18:44:25 -0700

    done

---
+++ ruby_book/hash/ex06.rb
@@ -0,0 +1,13 @@
+# hash_ex06.rb
+
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+=begin
+
+The first hash is using version 1.9 syntax.
+The second hash is using the older version syntax
+  AND is using a string as a key. The variable x is a string.
+
+=end
