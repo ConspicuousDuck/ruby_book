3656ddf389c1ce2f11431aaaae6a3d095934bd80
InconspicuousDuck <47161457+InconspicuousDuck@users.noreply.github.com>
2022-05-19 18:52:22 -0700

    done

---
+++ ruby_book/hash/ex07.rb
@@ -0,0 +1,24 @@
+# hash_ex07.rb
+
+=begin
+
+NoMethodError: undefined method `keys' for Array
+A. We're missing keys in an array variable.
+
+B. There is no method called keys for Array objects.
+
+C. keys is an Array object, but it hasn't been defined yet.
+
+D. There's an array of strings, and we're trying to get the string keys out of the array, but it doesn't exist.
+
+My answer: B. 
+I felt the error was a built in response
+specfically for built in methods. I interpretedt it meant
+there is no built in method 'keys' for the Array type.
+So I googled to see if there was a 'keys' method for Arrays.
+I didn' think so because it would not make sense because Arrays
+don't have keys. I was correct there is no keys method for arrays.
+
+
+
+=end
